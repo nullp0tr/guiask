a0b0c79d27f795f5ccf91a16f8da68922f0b99eb
Ahmed Alsharif <ahmeds2000x@gmail.com>
2018-01-09 19:38:13 +0100

    Added detection of arrow keys.

--- guiask/getch.py
+++ guiask/getch.py
@@ -13,8 +13,19 @@ def getchar():
         tty.setraw(sys.stdin.fileno())
         if sys.stdin in select.select([sys.stdin], [], [], 0.1)[0]:
             ch = sys.stdin.read(1)
-
+            if ch == '\x1b':
+                ch = ch + sys.stdin.read(1)
+                if ch[-1] == '[':
+                    ch = ch + sys.stdin.read(1)
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-
     return ch
+
+
+if __name__ == '__main__':
+    while True:
+        ch = getchar()
+        if ch:
+            print(repr(ch))
+        if ch == '\x03':
+            break
